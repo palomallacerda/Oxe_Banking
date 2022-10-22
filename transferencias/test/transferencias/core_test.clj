3f72d50b8f3df8a953d342f2acf8cf9b31a2baaf
palomalacerda <psls@ic.ufal.br>
2022-10-22 17:32:54 -0300

    Adding some changes

---
+++ transferencias/test/transferencias/core_test.clj
@@ -0,0 +1,7 @@
+(ns transferencias.core-test
+  (:require [clojure.test :refer :all]
+            [transferencias.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))

0048dbe325da0f8398eb824afb163cb105e3435f
PedroHkarate <83137278+PedroHkarate@users.noreply.github.com>
2022-12-03 14:51:41 -0300

    Update chaves_pix.clj

--- transferencias/src/transferencias/chaves_pix.clj
+++ transferencias/src/transferencias/chaves_pix.clj
@@ -6,25 +6,25 @@
         (let [chave_cpf (read-line)] ;criando chave com o numero de cpf
             (def cpfs_cadastrados
                 {:cpf_criado chave_cpf} ;inserindo cpf criado no mapa de cpfs cadastrados
-            )
-        )
-    )
+            )))
+
+    (defn criar_chave_cnpj []
+        (let [chave_cnpj (read-line)] ;criando chave com o numero de cnpj
+            (def cnpjs_cadastrados
+                {:cnpj_criado chave_cnpj} ;inserindo cpf criado no mapa de cnjps cadastrados
+            )))
 
     (defn criar_chave_email []
         (let [chave_email (read-line)] ;criando chave com o numero de email
             (def emails_cadastrados
                 {:email_criado chave_email} ;inserindo email criado no mapa de emails cadastrados
-            )
-        )
-    )
+            )))
 
     (defn criar_chave_celular []
         (let [chave_celular (read-line)] ;criando chave com o numero de celular
             (def celulares_cadastrados
                 {:celular_criado chave_celular} ;inserindo celular criado no mapa de celulares cadastrados
-            )
-        )
-    )
+            )))
 
     (defn criar_chave_aleatoria []
         (def chave_aleatorio (rand-int 99999999)) ;criando número aleatório como chave pix
@@ -35,26 +35,26 @@
     )
 
     (println "digite 1 para gerar chave com numero de CPF")
-    (println "digite 2 para gerar chave com endereco de e-mail")
-    (println "digite 3 para gerar chave com numero de celular")
-    (println "digite 4 para gerar chave com um numero aleatorio")
+    (println "digite 2 para gerar chave com numero de CNPJ")
+    (println "digite 3 para gerar chave com endereco de e-mail")
+    (println "digite 4 para gerar chave com numero de celular")
+    (println "digite 5 para gerar chave com um numero aleatorio")
     (let [chave (read-line)] ;chave
         (def numero (Integer/parseInt chave)) ;converter a chave escolhida para um tipo inteiro
         (if (== 1 numero) ;condicionais para saber o tipo de chave que foi escolhida
             ;(println "Opcao De Chave Escolhida: CPF")
             (let [novo_cpf (criar_chave_cpf)] novo_cpf)
             (if (== 2 numero)
-                ;(println "Opcao De Chave Escolhida: E-mail")
-                (let [novo_email (criar_chave_email)] novo_email)
+                ;(println "Opcao De Chave Escolhida: CNPJ")
+                (let [novo_cnpj (criar_chave_cnpj)] novo_cnpj)
+
                 (if (== 3 numero)
-                ;(println "Opcao De Chave Escolhida: Numero de Celular")
-                (let [novo_celular (criar_chave_celular)] novo_celular)
+                    ;(println "Opcao De Chave Escolhida: E-mail")
+                    (let [novo_email (criar_chave_email)] novo_email)
                     (if (== 4 numero)
-                        ;(println "Opcao De Chave Escolhida: Numero de Aleatorio")
-                        (let [novo_aleatorio (criar_chave_aleatoria)] novo_aleatorio)
-                    )
-                )
-            )
-        )
-    )
-)
+                    ;(println "Opcao De Chave Escolhida: Numero de Celular")
+                    (let [novo_celular (criar_chave_celular)] novo_celular)
+                        (if (== 5 numero)
+                            ;(println "Opcao De Chave Escolhida: Numero de Aleatorio")
+                            (let [novo_aleatorio (criar_chave_aleatoria)] novo_aleatorio)
+                        )))))))
